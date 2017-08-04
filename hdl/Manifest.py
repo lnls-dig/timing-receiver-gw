afda4eee874a3a5ec873d0ed87021609247f403f
Lucas Russo <lerwys@gmail.com>
2017-08-04 13:26:07 -0300

    hdl/Manifest.py: remove nonexistent etherbone/dsp-cores ip_cores

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -3,8 +3,6 @@
 modules = { "local": [
                 "modules",
                 "ip_cores/general-cores",
-                "ip_cores/etherbone-core",
-                "ip_cores/dsp-cores",
                 "ip_cores/infra-cores",
                 "platform"
             ]
