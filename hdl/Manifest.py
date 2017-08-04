c46477b8f8edbdfe225983cb0db77098b1899cd6
Lucas Russo <lerwys@gmail.com>
2017-08-04 13:28:56 -0300

    hdl/Manifest.py: add missing wr-cores to Manifest.py

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -3,6 +3,7 @@
 modules = { "local": [
                 "modules",
                 "ip_cores/general-cores",
+                "ip_cores/wr-cores",
                 "ip_cores/infra-cores",
                 "platform"
             ]
