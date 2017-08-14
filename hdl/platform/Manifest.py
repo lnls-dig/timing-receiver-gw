ff3c2d179b02f0a1c1fd4e426a334355f3905c2b
Lucas Russo <lerwys@gmail.com>
2017-08-14 17:17:52 -0300

    platform: add Vivado ILA component for artix7

--- hdl/platform/Manifest.py
+++ hdl/platform/Manifest.py
@@ -1,1 +1,11 @@
-files = []
+def __dirs():
+	dirs = []
+
+	if (target == "xilinx" and syn_device[0:4].upper()=="XC7A"):
+		dirs.extend(["artix7"]);
+	#else: #add paltform here and generate the corresponding ip cores
+	return dirs
+
+modules = {
+    "local" : __dirs()
+           }
---
+++ hdl/platform/artix7/ila/Manifest.py
@@ -0,0 +1,5 @@
+if (action == "synthesis"):
+    files = ["ila_4096_depth_64_width.xci"]
+else:
+    files = ["hdl/verilog/",
+            "sim/"]
